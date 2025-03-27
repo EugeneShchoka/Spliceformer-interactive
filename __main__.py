c687e30cca61703ff5eaaca4aec3f4b192a9124e
eugene <Shchoka.ev@phystech.edu>
2025-03-27 18:05:39 +0300

    Get SpliceAI-like values

--- __main__.py
+++ __main__.py
@@ -1,3 +1,7 @@
+from pkg_resources import resource_filename
+
+import pandas as pd
+
 import argparse
 import copy
 from pysam import VariantRecord, VariantFile
@@ -9,6 +13,8 @@ import torch.nn as nn
 
 from collections import OrderedDict
 from model import SpliceFormer
+
+import logging
 
 
 def get_options():
@@ -23,6 +29,12 @@ def get_options():
     )
     parser.add_argument(
         "-r", "--reference", required=True, help="Path to reference FASTA file (hg19 or hg38)"
+    )
+    parser.add_argument(
+        "-a", "--annotations", required=True,
+        help='"grch37" (GENCODE V24lift37 canonical annotation file in '
+             'package), "grch38" (GENCODE V24 canonical annotation file in '
+             'package), or path to a similar custom gene annotation file'
     )
 
     args = parser.parse_args()
@@ -157,22 +169,91 @@ def get_deltas(ref_prediction, alt_prediction, pos_s, crop, ref_len, alt_len, re
     return acceptorDelta[crop:-crop], donorDelta[crop:-crop]
 
 
+class Annotator:
+
+    def __init__(self, ref_fasta, annotations, device):
+
+        if annotations == 'grch37':
+            annotations = resource_filename(__name__, 'annotations/grch37.txt')
+        elif annotations == 'grch38':
+            annotations = resource_filename(__name__, 'annotations/grch38.txt')
+
+        try:
+            df = pd.read_csv(annotations, sep='\t', dtype={'CHROM': object})
+            self.genes = df['#NAME'].to_numpy()
+            self.chroms = df['CHROM'].to_numpy()
+            self.strands = df['STRAND'].to_numpy()
+            self.tx_starts = df['TX_START'].to_numpy() + 1
+            self.tx_ends = df['TX_END'].to_numpy()
+            self.exon_starts = [np.asarray([int(i) for i in c.split(',') if i]) + 1
+                                for c in df['EXON_START'].to_numpy()]
+            self.exon_ends = [np.asarray([int(i) for i in c.split(',') if i])
+                              for c in df['EXON_END'].to_numpy()]
+        except IOError as e:
+            logging.error('{}'.format(e))
+            exit()
+        except (KeyError, pd.errors.ParserError) as e:
+            logging.error('Gene annotation file {} not formatted properly: {}'.format(annotations, e))
+            exit()
+
+        try:
+            self.ref_fasta = Fasta(ref_fasta)
+        except IOError as e:
+            logging.error('{}'.format(e))
+            exit()
+
+        paths = ('models/spliceai{}.h5'.format(x) for x in range(1, 6))
+        self.models = [load_model(resource_filename(__name__, x), device) for x in paths]
+
+    def get_name_and_strand(self, chrom, pos):
+
+        chrom = normalise_chrom(chrom, list(self.chroms)[0])
+        idxs = np.intersect1d(np.nonzero(self.chroms == chrom)[0],
+                              np.intersect1d(np.nonzero(self.tx_starts <= pos)[0],
+                                             np.nonzero(pos <= self.tx_ends)[0]))
+
+        if len(idxs) >= 1:
+            return self.genes[idxs], self.strands[idxs], idxs
+        else:
+            return [], [], []
+
+    def get_pos_data(self, idx, pos):
+
+        dist_tx_start = self.tx_starts[idx] - pos
+        dist_tx_end = self.tx_ends[idx] - pos
+        dist_exon_bdry = min(np.union1d(self.exon_starts[idx], self.exon_ends[idx]) - pos, key=abs)
+        dist_ann = (dist_tx_start, dist_tx_end, dist_exon_bdry)
+
+        return dist_ann
+
+
 def main():
     args = get_options()
 
     SL = 5000
+    SL_SpliceAI = 100
     CL_max = 40000
+    CL_max_SpliceAI = 44900
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     models = load_model(CL_max, device)
 
-    with VariantFile(args.input, "r") as vcf:
-        fasta = Fasta(args.reference)
+    ann = Annotator(args.reference, args.annotations, device)
 
+    fasta = ann.ref_fasta
+
+    with VariantFile(args.input, "r") as vcf:
         with VariantFile(args.output, "w", header=vcf.header) as out:
+            header = vcf.header
+
+            header.add_line('##INFO=<ID=Spliceformer,Number=.,Type=String,Description="Spliceformer v1.0.0 custom '
+                            'annotation. These include delta scores (DS) and delta positions (DP) for '
+                            'acceptor gain (AG), acceptor loss (AL), donor gain (DG), and donor loss (DL). '
+                            'Format: SYMBOL|DS_AG|DS_AL|DS_DG|DS_DL|DP_AG|DP_AL|DP_DG|DP_DL">')
+
             for record in vcf:
-                delta_scores = []
+                delta_scores = []  # ???
 
                 chrom = record.chrom if record.chrom.startswith("chr") else f"chr{record.chrom}"
                 pos = record.pos
@@ -183,66 +264,80 @@ def main():
 
                 assert ref == ref_fa, f"Reference base mismatch at {chrom}:{pos} (expected {ref}, got {ref_fa})"
 
-                start, end = pos - SL // 2 - CL_max // 2, pos + SL // 2 + CL_max // 2
+                (genes, strands, idxs) = ann.get_name_and_strand(chrom, pos)
+
+                # if len(idxs) == 0:
+                #     return delta_scores  # add to sep function else closes a script
+
+                start, end = pos - SL // 2 - CL_max // 2, pos + SL // 2 + CL_max // 2  # mb + len-1
                 pos_start = pos - start
                 ref_seq = fasta[chrom][start - 1:end - 1].seq.upper()
                 ref_len = len(ref)
 
                 ref_seq_len = len(ref_seq)
 
-                ref_seq_encoded = one_hot_encode(ref_seq)
-
-                ref_seq_tensor = torch.tensor(ref_seq_encoded, dtype=torch.float32).T.unsqueeze(0).to(device)
-
-                ref_prediction = torch.stack([model(ref_seq_tensor)[0].detach() for model in models]).mean(
-                    dim=0).cpu().numpy()[0, :, :]
-
                 alt_number = len(alt)
                 for i in range(alt_number):
-                    alt_seq = ref_seq[:pos_start] + alt[i] + ref_seq[(pos_start + ref_len):]
+                    for i in range(len(idxs)):
+                        alt_seq = ref_seq[:pos_start] + alt[i] + ref_seq[(pos_start + ref_len):]
 
-                    alt_len = len(alt[i])
+                        alt_len = len(alt[i])
 
-                    alt_seq_len = len(alt_seq)
+                        alt_seq_len = len(alt_seq)
+                        if alt_seq_len > ref_seq_len:
+                            alt_seq = alt_seq[:ref_seq_len]
+                        elif alt_seq_len < ref_seq_len:
+                            alt_seq = alt_seq + fasta[chrom][end - 1:end - 1 + ref_seq_len - alt_seq_len].seq.upper()
 
-                    alt_seq_encoded = one_hot_encode(alt_seq)
+                        ref_seq_encoded = one_hot_encode(ref_seq)
+                        alt_seq_encoded = one_hot_encode(alt_seq)
 
-                    alt_seq_tensor = torch.tensor(alt_seq_encoded, dtype=torch.float32).T.unsqueeze(0).to(device)
+                        if strands[i] == '-':
+                            ref_seq_encoded = ref_seq_encoded[::-1, ::-1]
+                            alt_seq_encoded = alt_seq_encoded[::-1, ::-1]
 
-                    alt_prediction = torch.stack([model(alt_seq_tensor)[0].detach() for model in models]).mean(
-                        dim=0).cpu().numpy()[0, :, :]
+                        # get predictions
+                        ref_seq_tensor = torch.tensor(ref_seq_encoded.copy(), dtype=torch.float32).T.unsqueeze(0).to(device)
+                        alt_seq_tensor = torch.tensor(alt_seq_encoded.copy(), dtype=torch.float32).T.unsqueeze(0).to(device)
 
-                    acceptor_delta, donor_delta = get_deltas(ref_prediction, alt_prediction, pos_start, CL_max // 2,
-                                                             ref_len, alt_len, ref_seq_len, alt_seq_len)
+                        ref_prediction = torch.stack([model(ref_seq_tensor)[0].detach() for model in models]).mean(
+                            dim=0).cpu().numpy()[0, :, :]
+                        alt_prediction = torch.stack([model(alt_seq_tensor)[0].detach() for model in models]).mean(
+                            dim=0).cpu().numpy()[0, :, :]
 
-                    if ref_len == alt_len:
+                        if strands[i] == '-':
+                            ref_prediction = ref_prediction[:, ::-1]
+                            alt_prediction = alt_prediction[:, ::-1]
+
                         prediction = np.stack((ref_prediction, alt_prediction), axis=0)
-                    # elif ref_len > alt_len:
-                        #TODO
-                    # else:
-                        # TODO
 
-                    # acceptor
-                    idx_pa = (prediction[1, 1, :] - prediction[0, 1, :]).argmax()
-                    idx_na = (prediction[0, 1, :] - prediction[1, 1, :]).argmax()
+                        prediction_cropped = prediction[:, :, CL_max_SpliceAI // 2:-CL_max_SpliceAI // 2]
 
-                    # donor
-                    idx_pd = (prediction[1, 2, :] - prediction[0, 2, :]).argmax()
-                    idx_nd = (prediction[0, 2, :] - prediction[1, 2, :]).argmax()
+                        # acceptor
+                        idx_pa = (prediction_cropped[1, 1, :] - prediction_cropped[0, 1, :]).argmax()
+                        idx_na = (prediction_cropped[0, 1, :] - prediction_cropped[1, 1, :]).argmax()
+                        # donor
+                        idx_pd = (prediction_cropped[1, 2, :] - prediction_cropped[0, 2, :]).argmax()
+                        idx_nd = (prediction_cropped[0, 2, :] - prediction_cropped[1, 2, :]).argmax()
 
-                    # delta score
-                    DS_AG = (prediction[1, 1, idx_pa] - prediction[0, 1, idx_pa])
-                    DS_AL = (prediction[0, 1, idx_na] - prediction[1, 1, idx_na])
-                    DS_DG = (prediction[1, 2, idx_pd] - prediction[0, 2, idx_pd])
-                    DS_DL = (prediction[0, 2, idx_nd] - prediction[1, 2, idx_nd])
+                        # delta score
+                        DS_AG = (prediction_cropped[1, 1, idx_pa] - prediction_cropped[0, 1, idx_pa])
+                        DS_AL = (prediction_cropped[0, 1, idx_na] - prediction_cropped[1, 1, idx_na])
+                        DS_DG = (prediction_cropped[1, 2, idx_pd] - prediction_cropped[0, 2, idx_pd])
+                        DS_DL = (prediction_cropped[0, 2, idx_nd] - prediction_cropped[1, 2, idx_nd])
 
-                    # delta position
-                    DP_AG = idx_pa - SL // 2 - CL_max // 2
-                    DP_AL = idx_na - SL // 2 - CL_max // 2
-                    DP_DG = idx_pd - SL // 2 - CL_max // 2
-                    DP_DL = idx_nd - SL // 2 - CL_max // 2
+                        # delta position
+                        # DP_AG = idx_pa - SL // 2 - CL_max // 2
+                        # DP_AL = idx_na - SL // 2 - CL_max // 2
+                        # DP_DG = idx_pd - SL // 2 - CL_max // 2
+                        # DP_DL = idx_nd - SL // 2 - CL_max // 2
 
-                    a = 1
+                        DP_AG = idx_pa - SL_SpliceAI // 2
+                        DP_AL = idx_na - SL_SpliceAI // 2
+                        DP_DG = idx_pd - SL_SpliceAI // 2
+                        DP_DL = idx_nd - SL_SpliceAI // 2
+
+                        a = 1  # for debugging
 
 
 if __name__ == '__main__':
